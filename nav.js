ffbd0687371b1ccfa1900366647288f0b3cb51b1
hoibest <130141508+hcha3232@users.noreply.github.com>
2023-05-29 18:01:26 +1000

    main

--- nav.js
+++ nav.js
@@ -51,61 +51,27 @@ nav.innerHTML = `
 `
 
 navFooter.innerHTML = `
-<ul class="nav bg-light fixed-bottom" style="border-top: 1px rgb(226, 229, 230) solid; padding: 10px 20px 20px 20px;">
-  <li class="nav-item">
+<nav class="shadow-sm navbar navbar-light bg-light fixed-bottom nav-border" style="border-top: 1px solid #dddddd;">
+  <div class="container-fluid" style="padding: 0px 20px">
     <a class="nav-link active" aria-current="page" href="/">
-        Home
+      <div style="display:flex; flex-direction: column; align-items: center;"><i class="bi bi-house-door"></i>Home</div>
     </a>
-  </li>
-  <li class="nav-item">
+
+
     <a class="nav-link" href="#">
-      Criteria
+      <div style="display:flex; flex-direction: column; align-items: center;"><i class="bi bi-card-list"></i>Criteria</div>
     </a>
-  </li>
-</ul>
-`
 
-/*nav.innerHTML = `
+    <a class="nav-link" href="#">
+      <div style="display:flex; flex-direction: column; align-items: center;"><i class="bi bi-diagram-3"></i>Tree</div>
+    </a>
 
-<div class="container-fluid">
-  <a class="navbar-brand" href="/" style="font-weight: 600">Uveitis</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Uveitis Diagnostic Tree</h5>
-      <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-      <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Not Yet Developed</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-          Not Yet Developed
-          </a>
-          <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-      <form class="d-flex mt-3" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-success" type="submit">Search</button>
-      </form>
-    </div>
+    <a class="nav-link" href="#">
+      <div style="display:flex; flex-direction: column; align-items: center;"><i class="bi bi-info-circle"></i>Info</div>
+    </a>
   </div>
-</div>
+</nav>
+
+
 `
-*/
