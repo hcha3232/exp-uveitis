242c8a39340291fa452a143de568467a392f288d
hcha3232 <130141508+hcha3232@users.noreply.github.com>
2024-03-14 19:23:09 +1100

    some

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,30 @@
-// This is the "Offline page" service worker
+
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
 const CACHE = "pwabuilder-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPages = [
+  '/',
+  '/index.html',
+  '/html/au-exp.html',
+  '/html/iu-exp.html',
+  '/html/pu-exp.html',
+  '/html/classification.html',
+  '/css/style.css',
+  '/scripts/accor.js',
+  '/scripts/app-au.js',
+  '/scripts/app-iu.js',
+  '/scripts/app-pu.js',
+  '/scripts/nav.js',
+  '/scripts/new-decision.js',
+  '/scripts/phenotype.js',
+  '/scripts/phenotypeCriteria.js',
+  '/scripts/questions-pu.js',
+  '/scripts/questions.js'
+
+]
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -16,7 +35,7 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
     event.waitUntil(
       caches.open(CACHE)
-        .then((cache) => cache.add(offlineFallbackPage))
+        .then((cache) => cache.addAll(offlineFallbackPages))
     );
   });
   
@@ -39,7 +58,7 @@ self.addEventListener('install', async (event) => {
         } catch (error) {
   
           const cache = await caches.open(CACHE);
-          const cachedResp = await cache.match(offlineFallbackPage);
+          const cachedResp = await cache.match(offlineFallbackPages);
           return cachedResp;
         }
       })());
